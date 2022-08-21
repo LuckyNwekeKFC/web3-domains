3babcbb1b21a17970554a3e7a2a78b4c5c83071f
Lucky Nweke <kingfavourjudah@gmail.com>
2022-08-21 16:55:59 +0100

    update

---
+++ contracts/domains.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
+import "hardhat/console.sol";
+
+contract Domains {
+  // A "mapping" data type to store their names
+  mapping(string => address) public domains;
+
+  // Checkout our new mapping! This will store values
+  mapping(string => string) public records;
+  
+  constructor(string memory) payable{
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
+  }
+
+
+
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 1 * 10**17;
+    }
+  }
+
+  // A register function that adds their names to our mapping
+  function register(string calldata name) public {
+      domains[name] = msg.sender;
+      require(domains[name] == address(0));
+      domains[name] = msg.sender;
+      console.log("%s has registered a domain!", msg.sender);
+  }
+
+  // This will give us the domain owners' address
+  function getAddress(string calldata name) public view returns (address) {
+      return domains[name];
+  }
+
+    function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
+  }
+
+}
